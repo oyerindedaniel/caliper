0bedd082cc6443a8290f4decaf333122f5b1d385
oyerindedaniel <oyerinde.daniel@yahoo.com>
2026-02-07 20:59:18 +0100

    fix build

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -2,5 +2,4 @@ export * from "./core.js";
 export * from "./bridge.js";
 export * from "./audit.js";
 export * from "./serialization.js";
-export * from "./reconciler.js";
 export * from "./constants.js";
