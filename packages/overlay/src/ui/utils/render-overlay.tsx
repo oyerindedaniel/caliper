ba0e130773a4c598d6e4a45b24a1600ff67609f1
oyerindedaniel <oyerinde.daniel@yahoo.com>
2026-02-12 15:37:57 +0100

    revert src

--- packages/overlay/src/ui/utils/render-overlay.tsx
+++ packages/overlay/src/ui/utils/render-overlay.tsx
@@ -133,7 +133,7 @@ export function Overlay(props: OverlayProps) {
         <Portal mount={getOverlayRoot()}>
           <Calculator
             state={props.calculatorState!()!}
-            onClose={props.onCalculatorClose || (() => { })}
+            onClose={props.onCalculatorClose || (() => {})}
             position={{ x: props.cursor().x, y: props.cursor().y }}
             isFocused={props.activeFocus?.() === "calculator"}
           />
@@ -164,8 +164,8 @@ export function Overlay(props: OverlayProps) {
             projectionState={props.projectionState}
             metadata={props.selectionMetadata}
             result={props.result}
-            onUpdate={props.onRulerUpdate || (() => { })}
-            onRemove={props.onRulerRemove || (() => { })}
+            onUpdate={props.onRulerUpdate || (() => {})}
+            onRemove={props.onRulerRemove || (() => {})}
             onLineClick={props.onLineClick}
           />
         </Portal>
